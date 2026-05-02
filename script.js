9e9880c01db30a59f055f0c9a774e1aea6dce3c4
zearn-app <zoroz0673@gmail.com>
2026-05-02 13:18:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ camera.position.z = 500;
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0xffffff, 1); // white background
 document.body.appendChild(renderer.domElement);
 
 /* CONTROLS */
@@ -50,7 +49,7 @@ for (let i = 0; i < length; i += 0.1) {
   );
 }
 const geometry = new THREE.BufferGeometry().setFromPoints(vertices);
-const material = new THREE.PointsMaterial( { color: 0xee5282, size: 3 } );
+const material = new THREE.PointsMaterial( { color: 0xee5282,blending: THREE.AdditiveBlending, size: 3 } );
 const particles = new THREE.Points(geometry, material);
 // Offset the particles in the scene based on the viewbox values
 particles.position.x -= 600 / 2;
